91a420cbe38a1bf24389d3d3aa63a1d77f920951
Truong Tan Thuan <tanthuan.truong@citynow.com>
2020-03-04 17:33:51 +0700

    Ex6

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,23 @@
-import { useRouter } from 'next/router';
-import Layout from '../../components/MyLayout';
+import Layout from "../../components/MyLayout";
+import axios from "axios";
 
-export default function Post() {
-  const router = useRouter();
-
-  return (
+const Post = props => (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?[pb]>/g, "")}</p>
+        {props.show.image ? <img src={props.show.image.medium} /> : null}
     </Layout>
-  );
-}
+);
+
+Post.getInitialProps = async function(context) {
+    console.log("context", context);
+    const { id } = context.query;
+    const res = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.data;
+
+    console.log(`Fetched show: ${show.name}`);
+
+    return { show };
+};
+
+export default Post;
