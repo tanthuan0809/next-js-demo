08829a8f7fde6d44897fd16f7a0dc484cb3efc5c
Truong Tan Thuan <tanthuan.truong@citynow.com>
2020-03-04 16:34:20 +0700

    Ex4

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,9 @@
-import withLayout from '../components/MyLayout';
+import Layout from '../components/MyLayout.js'
 
-const Page = () => <p>This is the about pageeee</p>;
-
-export default withLayout(Page);
+export default function About() {
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  )
+}
---
+++ pages/post.js
@@ -0,0 +1,20 @@
+import Layout from '../components/MyLayout.js';
+import { useRouter } from 'next/router';
+
+const Content = () => {
+  const router = useRouter();
+  return (
+    <>
+      <h1>{router.query.title}</h1>
+      <p>This is the blog post content.</p>
+    </>
+  );
+};
+
+const Page = () => (
+  <Layout>
+    <Content />
+  </Layout>
+);
+
+export default Page;
