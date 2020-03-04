2c74790b58484015ff5693950f127ee862240f63
Truong Tan Thuan <tanthuan.truong@citynow.com>
2020-03-04 15:56:37 +0700

    Ex2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+import Header from "../comps/Header";
+
 export default function Index() {
     return (
         <div>
-            <h1>Hello TTT</h1>
+            <Header />
+            <p>Hello TTT</p>
+            
+            {/* <Link href="/about">
+                <a title="About Page">About Page</a>
+            </Link> */}
         </div>
     );
 }
